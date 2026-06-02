bb7841fcf7ca4f0e9105d57395bff339353769f3
cs <59759636+bchsekhar@users.noreply.github.com>
2026-06-02 06:32:49 +0000

    Update welcome message in Wish.java

--- cs_java/src/cs_java/Wish.java
+++ cs_java/src/cs_java/Wish.java
@@ -3,6 +3,8 @@ package cs_java;
 public class Wish {
 
 	public static void main(String[] args) {
-		System.out.println("Welcome");
+		System.out.println("Welcome ");
+				System.out.println("To HclTech
+
 	}
 }
